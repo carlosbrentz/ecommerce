fb67d65fdc89234fa44d170f609c7bc2f24d51e2
carlosbrentz <carlosbrentz@gmail.com>
2019-04-10 09:13:28 -0300

    meus pedidos e resumo carrinho

--- site.php
+++ site.php
@@ -264,15 +264,15 @@ $app->post("/checkout", function(){
 
   $cart = Cart::getFromSession();
 
-  $totals = $cart->getCalculateTotal();
-    
+  $cart->getCalculateTotal();
+ 
   $order->setData([
 
     'idcart'=>$cart->getidcart(),
     'idaddress'=>$address->getidaddress(),
     'iduser'=>$user->getiduser(),
     'idstatus'=>OrderStatus::EM_ABERTO,
-    'vltotal'=>$totals['vlprice'] + $cart->getVlfreight()
+    'vltotal'=>$cart->getvltotal()
 
   ]);
  
@@ -543,6 +543,7 @@ $app->get("/boleto/:idorder", function($idorder){
     $taxa_boleto = 5.00;
     $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
     $valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+    $valor_cobrado = str_replace(".", "",$valor_cobrado);
     $valor_cobrado = str_replace(",", ".",$valor_cobrado);
     $valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
@@ -601,5 +602,44 @@ $app->get("/boleto/:idorder", function($idorder){
 
 });
 
+$app->get("/profile/orders", function(){
+
+  User::verifyLogin(false);
+
+  $user = User::getFromSession();
+
+  $page = new Page();
+
+  $page->setTpl("profile-orders",[
+    'orders'=>$user->getOrders()
+  ]);
+
+});
+
+$app->get("/profile/orders/:idorder", function($idorder){
+
+  User::verifyLogin(false);
+
+  $order = new Order();
+
+  $order->get((int)$idorder);
+
+  $cart = new Cart();
+
+  $cart->get((int)$order->getidcart());
+
+  $cart->getCalculateTotal();
+
+  $page = new Page();
+
+  $page->setTpl("profile-orders-detail",[
+     'order'=>$order->getValues(),
+     'cart'=>$cart->getValues(),
+     'products'=>$cart->getProducts()
+  ]);
+
+
+
+});
 
  ?>
